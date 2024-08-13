de4dede7e61898e2443e0b257823c4f9ac158204
Oleg Bannyi <oleg.bannyi@gmail.com>
2024-08-13 17:44:30 +0300

    RSM | section 2 | lesson 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod balances;
 mod system;
 
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	pub fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	println!("Hello, world!");
 }
