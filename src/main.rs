a07559ea8b16e681ca94771d16b59b1a8ee1b387
Oleg Bannyi <oleg.bannyi@gmail.com>
2024-08-13 19:18:59 +0300

    RSM | section 2 | lesson 4

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,24 @@ impl Runtime {
 }
 
 fn main() {
-	println!("Hello, world!");
+	let mut runtime = Runtime::new();
+
+	let alice = String::from("alice");
+	let bob = String::from("bob");
+	let charlie = String::from("charlie");
+
+	runtime.system.inc_block_number();
+	runtime.balances.set_balance(&alice, 100);
+
+	runtime.system.inc_nonce(&alice);
+	let _res = runtime
+		.balances
+		.transfer(alice.clone(), bob.clone(), 30)
+		.map_err(|e| eprintln!("Error: {:?}", e));
+
+	runtime.system.inc_nonce(&alice);
+	let _res = runtime
+		.balances
+		.transfer(alice.clone(), charlie.clone(), 20)
+		.map_err(|e| eprintln!("Error: {:?}", e));
 }
