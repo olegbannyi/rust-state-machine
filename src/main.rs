2d2a567790c80620cdb0063d562ee1c68458c5f3
Oleg Bannyi <oleg.bannyi@gmail.com>
2024-08-15 11:59:10 +0300

    RSM | section 4 | lesson 2

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ mod support;
 mod system;
 
 mod types {
+    use crate::{support, RuntimeCall};
+
 	pub type AccountId = String;
 	pub type Balance = u128;
 	pub type BlockNumber = u32;
 	pub type Nonce = u32;
+	pub type Extrinsic = support::Extrinsic<AccountId, RuntimeCall>;
+	pub type Header = support::Header<BlockNumber>;
+	pub type Block = support::Block<Header, Extrinsic>;
+}
+
+enum RuntimeCall {
+	
 }
 
 #[derive(Debug)]
